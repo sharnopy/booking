7d510c61f6e6954805b0fb6efae89bda7f399c8b
omar <omarnabilsharnopu@gmail.cm>
2023-12-24 20:37:13 +0200

    splashUi

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookapp/constants/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import 'features/splash/presentation/viwes/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
